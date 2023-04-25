a30c9b06ae4c99b352dba39edc20ede70137ad29
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-04-26 00:23:11 +0200

    requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,14 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
-# import pytest
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
-# def test_encrypt_message():
+def test_encrypt_message():
 
-#     assert encrypt_message('trybe', 2) == ('eby_rt')
+    assert encrypt_message('trybe', 2) == ('eby_rt')
 
-#     assert encrypt_message('trybe', 3) == ('eby_rt')
+    assert encrypt_message('trybe', 3) == ('yrt_eb')
 
-#     assert encrypt_message('trybe', -10) == ('ebyrt')
+    assert encrypt_message('trybe', -10) == ('ebyrt')
 
-#     with pytest.raises(ValueError):
-#         encrypt_message('trybe', 99)
+    with pytest.raises(TypeError):
+        encrypt_message(99, 'trybe')
