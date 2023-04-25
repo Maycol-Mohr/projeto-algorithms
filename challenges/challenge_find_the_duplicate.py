016754c41b2aeedafb7a219e3c2a93915c04ec92
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-04-26 01:24:19 +0200

    tentando requisito 5 com ajuda

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,15 +1,22 @@
-# def find_duplicate(nums):
-#     for num in nums:
-#         if type(num) != int:
-#             return False
+def find_duplicate(nums):
+    array_vazio = {}
 
-#         if num < 1:
-#             return False
+    for num in nums:
+        if type(num) != int:
+            return False
 
-#         if nums.count(num) > 1:
-#             return num
+        if num < 1:
+            return False
 
-#     return False
+        if num not in array_vazio:
+            array_vazio[num] = num
+        else:
+            return num
+
+        # if nums.count(num) > 1:
+        #     return num
+
+    return False
 
 
-# print(find_duplicate([3, 1, 2, 4, 6, 5, 7, 7, 7, 8]))
+print(find_duplicate([3, 1, 2, 4, 6, 5, 7, 7, 7, 8]))
