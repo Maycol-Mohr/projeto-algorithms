a3ffc1fbb8491735f2b11138e12d1eab8b79cd9e
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-04-26 00:12:12 +0200

    tentando exercicio 6

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,15 +1,15 @@
-def find_duplicate(nums):
-    for num in nums:
-        if type(num) != int:
-            return False
+# def find_duplicate(nums):
+#     for num in nums:
+#         if type(num) != int:
+#             return False
 
-        if num < 1:
-            return False
+#         if num < 1:
+#             return False
 
-        if nums.count(num) > 1:
-            return num
+#         if nums.count(num) > 1:
+#             return num
 
-    return False
+#     return False
 
 
-print(find_duplicate([3, 1, 2, 4, 6, 5, 7, 7, 7, 8]))
+# print(find_duplicate([3, 1, 2, 4, 6, 5, 7, 7, 7, 8]))
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
 # from challenges.challenge_encrypt_message import encrypt_message
+# import pytest
 
 
-def test_encrypt_message():
-    pass
+# def test_encrypt_message():
+
+#     assert encrypt_message('trybe', 2) == ('eby_rt')
+
+#     assert encrypt_message('trybe', 3) == ('eby_rt')
+
+#     assert encrypt_message('trybe', -10) == ('ebyrt')
+
+#     with pytest.raises(ValueError):
+#         encrypt_message('trybe', 99)
