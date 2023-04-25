80a820ca380ff110b23d7df912e9da49cea3b7f5
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-04-25 19:55:31 +0200

    tentando exercicios 1 e 3 com ajuda

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,20 @@
+period = [('2', 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None or target_time == '':
+        return None
+
+    quantity = 0
+
+    for start, end in permanence_period:
+        if type(start) != int or type(end) != int:
+            return None
+
+        if start <= target_time and end >= target_time:
+            quantity = quantity + 1
+
+    return quantity
+
+
+print(study_schedule(period, 2))
