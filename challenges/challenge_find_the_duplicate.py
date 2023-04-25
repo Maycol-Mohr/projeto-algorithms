ee4da6024cebc8e936642c69794118395cd8c20d
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-04-25 23:22:56 +0200

    tentando exercicio 5 com ajuda

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,15 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    for num in nums:
+        if type(num) != int:
+            return False
+
+        if num < 1:
+            return False
+
+        if nums.count(num) > 1:
+            return num
+
+    return False
+
+
+print(find_duplicate([3, 1, 2, 4, 6, 5, 7, 7, 7, 8]))
