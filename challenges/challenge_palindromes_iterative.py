f286da64d321075cbf5ada5f1c925c119d4d0a47
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-04-25 23:49:58 +0200

    requisito 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,13 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == '':
+        return False
+
+    reversed_word = word[len(word)::-1]
+
+    if word == reversed_word:
+        return True
+    else:
+        return False
+
+
+print(is_palindrome_iterative('asa'))
